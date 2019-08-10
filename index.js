a10b015c1628f6260f42a3e9b8a812f51d992c54
kv-2 <str_token@mail.ru>
2019-08-10 16:46:58 +0300

    add simples routers

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const app = express();
+const articles = [{
+  title: 'Example'
+}];
 
-const port = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.set('port', process.env.PORT || 3000);
+
+// gets all articles
+app.get('/articles', (req, res, next) => {
+  res.send(articles);
 });
-app.listen(port, () => {
-  console.log(`Express web app aviailable at localhost:${port}`);
+// creates an artocles
+app.post('/articles', (req, res, next) => {
+  res.send('OK');
 });
+// gets a single article
+app.get('/articles/:id', (req, res, next) => {
+  const id = req.params.id;
+  console.log('Fetching:', id);
+  res.send(articles[id]);
+});
+//delete an article
+app.get('/articles', (req, res, next) => {
+  const id = req.params.id;
+  console.log('Delete:', id);
+  delete articles[id];
+  res.send({
+    message: 'Deleted!'
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('App started on port:', app.get('port'));
+});
+
+module.exports = app;
