fb8647281c413bd6fc150fd7690bbe43f3fbd795
kv-2 <str_token@mail.ru>
2019-08-10 18:05:12 +0300

    implement body-parser to make POST

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const app = express();
 const articles = [{
   title: 'Example'
 }];
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
+
+//support request bodies encoded as JSON
+app.use(bodyParser.json());
+//support form-encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // gets all articles
 app.get('/articles', (req, res, next) => {
@@ -12,7 +20,11 @@ app.get('/articles', (req, res, next) => {
 });
 // creates an artocles
 app.post('/articles', (req, res, next) => {
-  res.send('OK');
+  const article = {
+    title: req.body.title
+  };
+  articles.push(article);
+  res.send(article);
 });
 // gets a single article
 app.get('/articles/:id', (req, res, next) => {
@@ -21,7 +33,7 @@ app.get('/articles/:id', (req, res, next) => {
   res.send(articles[id]);
 });
 //delete an article
-app.get('/articles', (req, res, next) => {
+app.delete('/articles/:id', (req, res, next) => {
   const id = req.params.id;
   console.log('Delete:', id);
   delete articles[id];
